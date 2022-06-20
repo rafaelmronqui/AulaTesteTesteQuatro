704516829128285dcec7f723f1ac5b3ed7f96b61
rafaelronqui <37422164+rafaelronqui@users.noreply.github.com>
2022-06-20 17:36:04 -0300

    aaaaaaaaaa

--- AulaTesteTesteQuatro/src/teste/Programa.java
+++ AulaTesteTesteQuatro/src/teste/Programa.java
@@ -5,7 +5,9 @@ public class Programa {
 	public static void main(String[] args) {
 
 		System.out.println("Testeee\n.");
-		System.out.println("Testeee\n.");
+		System.out.println("Testeeeaaaaaaa\n.");
+		System.out.println("Testeeeeaaaee");
+		System.out.println("aaaa");
 
 	}
 
---
+++ AulaTesteTesteQuatro/src/teste/ProgramaDois.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class ProgramaDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste");
+	}
+
+}
